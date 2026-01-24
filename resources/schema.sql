3d8746ce66b1894941ebf86028057dee99ec5265
santsane <junjunsanji@gmail.com>
2026-01-24 22:44:30 +0500

    feat(schema): add PostgreSQL schema

---
+++ resources/schema.sql
@@ -0,0 +1,17 @@
+-- Users Table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(20) NOT NULL, -- 'CANDIDATE' or 'RECRUITER'
+    additional_info VARCHAR(255)
+);
+
+-- Jobs Table
+CREATE TABLE jobs (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    recruiter_id INT,
+    FOREIGN KEY (recruiter_id) REFERENCES users(id) ON DELETE CASCADE
+);
