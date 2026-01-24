b5b3874e33dbd3cca175b817e3023b768c0d664b
santsane <junjunsanji@gmail.com>
2026-01-24 21:46:52 +0500

    feat(service): add validation and business rules

---
+++ src/com/jrs/service/UserService.java
@@ -0,0 +1,44 @@
+package com.jrs.service;
+
+import com.jrs.exception.DuplicateResourceException;
+import com.jrs.exception.InvalidInputException;
+import com.jrs.model.User;
+import com.jrs.repository.UserRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService {
+    // Composition
+    private final UserRepository userRepository = new UserRepository();
+
+    public void registerUser(User user) throws InvalidInputException, SQLException {
+        // Validation
+        if (user.getName() == null || user.getName().trim().isEmpty()) {
+            throw new InvalidInputException("Validation Failed: Empty name");
+        }
+
+        if (user.getEmail() == null || !user.getEmail().contains("@")) {
+            throw new InvalidInputException("Validation Failed: Invalid email");
+        }
+
+        // Business
+        // Unique email check
+        List<User> existingUsers = userRepository.getAll();
+        for (User u : existingUsers) {
+            if (u.getEmail().equalsIgnoreCase(user.getEmail())) {
+                throw new DuplicateResourceException("Business Rule Violated: Email " + user.getEmail() + " taken");
+            }
+        }
+
+        userRepository.create(user);
+    }
+
+    public List<User> listAllUsers() throws SQLException {
+        return userRepository.getAll();
+    }
+
+    public void removeUser(int id) throws SQLException {
+        userRepository.delete(id);
+    }
+}
