603189996a1fe43767e6aa432b12e8453c0b31a6
santsane <junjunsanji@gmail.com>
2026-02-01 21:44:21 +0500

    refactor(service): add dependency inversion and move business logic

--- src/com/jrs/service/UserService.java
+++ src/com/jrs/service/UserService.java
@@ -3,42 +3,39 @@ package com.jrs.service;
 import com.jrs.exception.DuplicateResourceException;
 import com.jrs.exception.InvalidInputException;
 import com.jrs.model.User;
-import com.jrs.repository.UserRepository;
+import com.jrs.repository.interfaces.CrudRepository;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
-    // Composition
-    private final UserRepository userRepository = new UserRepository();
+    // Dependency Inversion- Depends on (CrudRepository) not Concretion
+    private final CrudRepository<User> userRepository;
 
-    public void registerUser(User user) throws InvalidInputException, SQLException {
-        // Validation
-        if (user.getName() == null || user.getName().trim().isEmpty()) {
-            throw new InvalidInputException("Validation Failed: Empty name");
+    // Constructor Injection
+    public UserService(CrudRepository<User> userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void registerUser(User user) throws InvalidInputException, SQLException, DuplicateResourceException {
+        // Validation Logic
+        if (!user.isValid()) {
+            throw new InvalidInputException("Invalid User Data: Name or Email is incorrect.");
         }
 
-        if (user.getEmail() == null || !user.getEmail().contains("@")) {
-            throw new InvalidInputException("Validation Failed: Invalid email");
-        }
-
-        // Business
-        // Unique email check
+        // Business Logic- check duplicates
         List<User> existingUsers = userRepository.getAll();
         for (User u : existingUsers) {
             if (u.getEmail().equalsIgnoreCase(user.getEmail())) {
-                throw new DuplicateResourceException("Business rule broken: Email " + user.getEmail() + " taken");
+                throw new DuplicateResourceException("User with email " + user.getEmail() + " already exists.");
             }
         }
 
+        // Persistence
         userRepository.create(user);
     }
 
-    public List<User> listAllUsers() throws SQLException {
+    public List<User> getAllUsers() throws SQLException {
         return userRepository.getAll();
-    }
-
-    public void removeUser(int id) throws SQLException {
-        userRepository.delete(id);
     }
 }
